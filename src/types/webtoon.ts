1482d92a87a5a65c8cc07f46c5ea076e30fd2fd2
artinc5233 <artinc5233@gmail.com>
2026-01-19 23:38:50 +0900

    feat: 웹툰 타입 정의 확장 (상세 정보, 에피소드 등)

--- src/types/webtoon.ts
+++ src/types/webtoon.ts
@@ -11,3 +11,29 @@ export interface Webtoon {
   isNew: boolean;
   viewCount: number;
 }
+
+export interface Episode {
+  id: number;
+  episodeNumber: number;
+  title: string;
+  thumbnail: string;
+  uploadDate: string;
+  viewCount: number;
+  likeCount: number;
+  commentCount: number;
+  isFree: boolean;
+}
+
+export interface WebtoonDetail extends Webtoon {
+  authorComment: string;
+  bannerImage: string;
+  longDescription: string;
+  subscriberCount: number;
+  rating: number;
+  ratingCount: number;
+  totalEpisodes: number;
+  isCompleted: boolean;
+  firstEpisodeDate: string;
+  latestEpisodeDate: string;
+  episodes: Episode[];
+}
